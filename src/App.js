2dafbdb2483a3e73a40723929c58c38f05ae8571
chenzizhao <zizhao.chen@zilliz.com>
2019-12-26 15:29:48 +0800

    add animation

--- src/App.js
+++ src/App.js
@@ -4,33 +4,47 @@ import Login from "pages/Login";
 import TablePage from "pages/table";
 import AdvancedPage from "pages/advanced";
 import HardwarePage from "pages/hardware";
-
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import "./app.scss";
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/manage">
-            <Layout>
-              <Route path="/manage/table">
-                <TablePage></TablePage>
-              </Route>
-              <Route path="/manage/advanced">
-                <AdvancedPage></AdvancedPage>
-              </Route>
-              <Route path="/manage/hardware">
-                <HardwarePage></HardwarePage>
-              </Route>
-              <Route path="/manage/index">index</Route>
-            </Layout>
-          </Route>
-          <Route path="/">
-            <Login></Login>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <HashRouter>
+      <Route
+        render={({ location }) => (
+          <Switch location={location}>
+            <Route path="/manage">
+              <Layout>
+                <TransitionGroup>
+                  <CSSTransition
+                    key={location.pathname}
+                    timeout={{ enter: 300, exit: 100 }}
+                    classNames="fade"
+                  >
+                    <section className="route-section">
+                      <Switch location={location}>
+                        <Route path="/manage/table">
+                          <TablePage></TablePage>
+                        </Route>
+                        <Route path="/manage/advanced">
+                          <AdvancedPage></AdvancedPage>
+                        </Route>
+                        <Route path="/manage/hardware">
+                          <HardwarePage></HardwarePage>
+                        </Route>
+                        <Route path="/manage/index">index</Route>
+                      </Switch>
+                    </section>
+                  </CSSTransition>
+                </TransitionGroup>
+              </Layout>
+            </Route>
+            <Route path="/">
+              <Login></Login>
+            </Route>
+          </Switch>
+        )}
+      />
+    </HashRouter>
   );
 }
