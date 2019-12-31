c057430728c60f6a2b8988167aa76b8b2788b07a
chenzizhao <zizhao.chen@zilliz.com>
2019-12-31 15:06:07 +0800

    fix button style

--- electron-starter.js
+++ electron-starter.js
@@ -13,7 +13,7 @@ let mainWindow;
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 1000, height: 800 });
+  mainWindow = new BrowserWindow({ width: 1200, height: 800 });
   // mainWindow.maximize();
   // mainWindow.show();
   // and load the index.html of the app.
