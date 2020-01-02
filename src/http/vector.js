c7fe5ac1a7e15b17aae4d18b2ccdc20095cf46d1
chenzizhao <zizhao.chen@zilliz.com>
2020-01-02 16:41:23 +0800

    fix ui

--- src/http/vector.js
+++ src/http/vector.js
@@ -4,6 +4,5 @@ export async function searchVectors(data = {}) {
   const copyData = JSON.parse(JSON.stringify(data));
   delete copyData.tableName;
   const res = await http.put(`/tables/${data.tableName}/vectors`, copyData);
-
   return res.data;
 }
