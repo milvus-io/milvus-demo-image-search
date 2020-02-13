ca99a7bdfe081ecfd64157c06157d1365304191e
nameczz <nameczz@nameczzdeMacBook-Pro.local>
2020-02-13 18:25:15 +0800

    test workflow

--- src/index.js
+++ src/index.js
@@ -11,3 +11,5 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+
+console.log("test")
