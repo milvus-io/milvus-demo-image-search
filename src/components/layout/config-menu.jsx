6a958fb9a01c561328c38191e9ef02a142c7c76f
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2020-02-29 13:33:00 +0800

    config menu styling

--- src/components/layout/config-menu.jsx
+++ src/components/layout/config-menu.jsx
@@ -1,14 +1,87 @@
-import React, { useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
-const ConfigMenu = props => {
-  const history = useHistory()
-  useEffect(() => {
-    history.push('/configs/storage')
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  return (
-    <div>Config Menu</div>
-  )
-}
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { FaNetworkWired, FaRegFolder } from "react-icons/fa";
+import { FiCpu } from "react-icons/fi";
+import { GiChart } from "react-icons/gi";
+import { GoNote, GoSettings } from "react-icons/go";
 
-export default ConfigMenu
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    maxWidth: 360
+  },
+  icon: {
+    minWidth: theme.spacing(5),
+    "& svg": {
+      width: 24,
+      height: 24,
+      fill: "#FFF"
+    }
+  },
+  item: {
+    paddingLeft: 0
+  }
+}));
+
+const ConfigMenu = props => {
+  const history = useHistory();
+  const classes = useStyles();
+
+  useEffect(() => {
+    history.push("/configs/storage");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div className={classes.root}>
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItem button className={classes.item}>
+          <ListItemIcon className={classes.icon}>
+            <FaNetworkWired />
+          </ListItemIcon>
+          <ListItemText primary="Network Access" />
+        </ListItem>
+        <ListItem button className={classes.item}>
+          <ListItemIcon className={classes.icon}>
+            <FaRegFolder />
+          </ListItemIcon>
+          <ListItemText primary="Storage Path" />
+        </ListItem>
+
+        <ListItem button className={classes.item}>
+          <ListItemIcon className={classes.icon}>
+            <GoSettings />
+          </ListItemIcon>
+          <ListItemText primary="Advanced Settings" />
+        </ListItem>
+
+        <ListItem button className={classes.item}>
+          <ListItemIcon className={classes.icon}>
+            <FiCpu />
+          </ListItemIcon>
+          <ListItemText primary="Hardware Resources" />
+        </ListItem>
+
+        <ListItem button className={classes.item}>
+          <ListItemIcon className={classes.icon}>
+            <GiChart />
+          </ListItemIcon>
+          <ListItemText primary="Metrics" />
+        </ListItem>
+
+        <ListItem button className={classes.item}>
+          <ListItemIcon className={classes.icon}>
+            <GoNote />
+          </ListItemIcon>
+          <ListItemText primary="ELK" />
+        </ListItem>
+      </List>
+    </div>
+  );
+};
+
+export default ConfigMenu;
