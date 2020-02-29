977f1edde76184c673f4e16db8ce5b33cee1a6d1
AzAz <az15921869329@gmail.com>
2020-02-29 15:50:33 +0800

    hardware ui basic done

--- src/pages/hardware/index.jsx
+++ src/pages/hardware/index.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { usePageStyles } from "../../hooks/page";
+import Typography from '@material-ui/core/Typography';
+import WithTip from "components/with-tip";
 import HardwareForm from "./form";
-import "./index.less";
+import PaperWrapper from "../../components/page-wrapper";
 const Hardware = props => {
+  const classes = usePageStyles();
   const { t } = useTranslation();
-  const configTrans = t("config");
+  const hardware = t("hardware");
 
   return (
-    <div className="hardware-wrapper">
-      <div className="header">
-        <h2>{configTrans.hardware}</h2>
-      </div>
-
-      <HardwareForm></HardwareForm>
+    <div className={classes.root}>
+      <PaperWrapper className={classes.paper}>
+        <div className={classes.titleContainer}>
+          <Typography variant={"h5"}>{hardware.title}</Typography>
+          <WithTip title={t("restartNotify")} placement="right"></WithTip>
+        </div>
+        <HardwareForm></HardwareForm>
+      </PaperWrapper>
     </div>
-  );
+  )
 };
 
 export default Hardware;
