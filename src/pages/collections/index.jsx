127eeb87e938940d8d8e0735d1f2889e670dc726
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2020-03-04 14:28:50 +0800

    some ux changes

--- src/pages/collections/index.jsx
+++ src/pages/collections/index.jsx
@@ -8,77 +8,74 @@ import { useTranslation } from "react-i18next";
 import { httpContext } from "../../context/http";
 import { dataManagementContext } from "../../context/data-management";
 
-import { materialContext } from '../../context/material'
-import CreateCollection from '../../components/dialogs/CreateCollection'
-import CreateIndex from '../../components/dialogs/CreateIndex'
+import { materialContext } from "../../context/material";
+import CreateCollection from "../../components/dialogs/CreateCollection";
+import CreateIndex from "../../components/dialogs/CreateIndex";
 
 const PAGE_SIZE = 10;
 const Collections = props => {
-  const {
-    deleteTable,
-    searchTable,
-    createTable,
-    createIndex
-  } = useContext(httpContext);
+  const { deleteTable, searchTable, createTable, createIndex } = useContext(
+    httpContext
+  );
 
-  const { setDialog, openSnackBar } = useContext(materialContext)
-  const { setRefresh, allCollections } = useContext(dataManagementContext)
+  const { setDialog, openSnackBar } = useContext(materialContext);
+  const { setRefresh, allCollections } = useContext(dataManagementContext);
   const classes = useDataPageStyles();
   const { t } = useTranslation();
   const tableTrans = t("table");
   const dataManageTrans = t("dataManage");
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
   const [offset, setOffset] = useState(0);
   const [count, setCount] = useState(0);
   const [current, setCurrent] = useState(0);
   const [isSearch, setIsSearch] = useState(false);
 
   const handleDelete = async (e, selected) => {
-    const res = await Promise.all(selected.map(async (v, i) => {
-      try {
-        return await deleteTable(v.table_name)
-
-      } catch (error) {
-        return error.response
-      }
-    }));
-    const errorMsg = res.filter(v => v)
+    const res = await Promise.all(
+      selected.map(async (v, i) => {
+        try {
+          return await deleteTable(v.table_name);
+        } catch (error) {
+          return error.response;
+        }
+      })
+    );
+    const errorMsg = res.filter(v => v);
 
     if (errorMsg.length) {
-      openSnackBar(errorMsg[0].data.message || 'Some Table Fail', 'error')
+      openSnackBar(errorMsg[0].data.message || "Some Table Fail", "error");
     } else {
-      openSnackBar(tableTrans.delete)
+      openSnackBar(tableTrans.delete);
     }
-    saveSuccess()
+    saveSuccess();
   };
 
   const saveSuccess = txt => {
-    setRefresh(true)
-    setOffset(0)
+    setRefresh(true);
+    setOffset(0);
     setCurrent(0);
   };
 
   useEffect(() => {
     if (isSearch) {
-      return
+      return;
     }
-    setData(allCollections.slice(offset, PAGE_SIZE + offset))
-    setCount(allCollections.length)
-  }, [allCollections, offset, isSearch])
+    setData(allCollections.slice(offset, PAGE_SIZE + offset));
+    setCount(allCollections.length);
+  }, [allCollections, offset, isSearch]);
 
   const handleSearch = async name => {
     setCurrent(0);
     if (!name) {
-      setOffset(0)
-      setIsSearch(false)
+      setOffset(0);
+      setIsSearch(false);
       return;
     }
     const res = (await searchTable(name)) || {};
-    setData([{ ...res, key: res.table_name }])
+    setData([{ ...res, key: res.table_name }]);
     setCount(1);
-    setIsSearch(true)
-
+    setIsSearch(true);
   };
 
   const handlePageChange = async (e, page) => {
@@ -86,15 +83,17 @@ const Collections = props => {
     setCurrent(page);
   };
 
-  const deleteIndex = async (collectionName) => {
-    const res = await createIndex(collectionName, { index_type: "FLAT", nlist: 16384 })
-    console.log(res)
+  const deleteIndex = async collectionName => {
+    const res = await createIndex(collectionName, {
+      index_type: "FLAT",
+      nlist: 16384
+    });
+    console.log(res);
     if (res && res.code === 0) {
-      saveSuccess()
-      openSnackBar(t('index').deleteSuccess)
+      saveSuccess();
+      openSnackBar(t("index").deleteSuccess);
     }
-  }
-
+  };
 
   const colDefinitions = [
     {
@@ -139,31 +138,42 @@ const Collections = props => {
     //   disabled: false
     // },
     {
-      label: "Create",
+      label: "Create Collection",
       icon: "create",
-      onClick: () => setDialog({
-        open: true,
-        type: 'custom',
-        params: {
-          component: <CreateCollection createTable={createTable} saveSuccess={saveSuccess}></CreateCollection>,
-        }
-      }),
+      onClick: () =>
+        setDialog({
+          open: true,
+          type: "custom",
+          params: {
+            component: (
+              <CreateCollection
+                createTable={createTable}
+                saveSuccess={saveSuccess}
+              ></CreateCollection>
+            )
+          }
+        }),
       disabled: selected => selected.length > 2
     },
     {
       label: "Delete",
       icon: "delete",
       onClick: (e, selected) => {
-        setDialog({
-          open: true,
-          type: 'notice',
-          params: {
-            title: `Do you want to delete collections ?`,
-            confirm: () => {
-              handleDelete(e, selected)
+        return new Promise(resolve => {
+          setDialog({
+            open: true,
+            type: "notice",
+            params: {
+              title: `Do you want to delete ${
+                selected.length === 1 ? `this item` : `these items`
+              }?`,
+              confirm: async () => {
+                await handleDelete(e, selected);
+                resolve(true);
+              }
             }
-          }
-        })
+          });
+        });
       },
       disabled: selected => selected.length === 0,
       disabledTooltip: "You can not delete this item"
@@ -174,11 +184,17 @@ const Collections = props => {
       onClick: (e, selected) => {
         setDialog({
           open: true,
-          type: 'custom',
+          type: "custom",
           params: {
-            component: <CreateIndex createIndex={createIndex} collectionInfo={selected[0]} saveSuccess={saveSuccess}></CreateIndex>,
+            component: (
+              <CreateIndex
+                createIndex={createIndex}
+                collectionInfo={selected[0]}
+                saveSuccess={saveSuccess}
+              ></CreateIndex>
+            )
           }
-        })
+        });
       },
       disabled: selected => selected.length !== 1,
       disabledTooltip: "You can not create index on multiple collections"
@@ -189,16 +205,17 @@ const Collections = props => {
       onClick: (e, selected) => {
         setDialog({
           open: true,
-          type: 'notice',
+          type: "notice",
           params: {
             title: `Do you want to delete index in ${selected[0].table_name}?`,
             confirm: () => {
-              deleteIndex(selected[0].table_name)
+              deleteIndex(selected[0].table_name);
             }
           }
-        })
+        });
       },
-      disabled: selected => selected.length !== 1 || selected[0].index === 'FLAT',
+      disabled: selected =>
+        selected.length !== 1 || selected[0].index === "FLAT"
     },
     {
       label: "",
@@ -206,16 +223,16 @@ const Collections = props => {
       searchText: "",
       onSearch: handleSearch,
       onClear: () => {
-        handleSearch('')
+        handleSearch("");
       }
     }
   ];
 
   const rows = data || [];
-  console.log(rows)
+  console.log(rows);
   return (
     <div className={`${classes.root}`}>
-      <PaperWrapper className={classes.paper} >
+      <PaperWrapper className={classes.paper}>
         <Box p={2}>
           <MilvusGrid
             title={[dataManageTrans.collections]}
