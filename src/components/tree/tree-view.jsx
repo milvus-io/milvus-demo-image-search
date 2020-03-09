1f84200115b4705b1578917807c182f0197fddf0
chenzizhao <zizhao.chen@zilliz.com>
2020-03-09 18:46:30 +0800

    lazy tree

--- src/components/tree/tree-view.jsx
+++ src/components/tree/tree-view.jsx
@@ -1,80 +1,101 @@
-import React from 'react';
-import { makeStyles, CircularProgress } from '@material-ui/core';
-import { TreeView } from '@material-ui/lab';
-import { ArrowDropDown, ArrowRight } from '@material-ui/icons';
-import { IoMdRefresh } from 'react-icons/io'
-import { MdStorage } from 'react-icons/md';
+import React from "react";
+import {
+  makeStyles,
+  CircularProgress,
+  Button,
+  Typography
+} from "@material-ui/core";
+import { TreeView } from "@material-ui/lab";
+import { ArrowDropDown, ArrowRight } from "@material-ui/icons";
+import { FiPlus } from "react-icons/fi";
 
-import StyledTreeItem from './tree-item'
+import { IoMdRefresh } from "react-icons/io";
+import { MdStorage } from "react-icons/md";
+
+import StyledTreeItem from "./tree-item";
 
 const useStyles = makeStyles(theme => ({
   root: {
     height: 264,
     flexGrow: 1,
     maxWidth: 400,
-    '& .MuiTreeItem-iconContainer': {
+    "& .MuiTreeItem-iconContainer": {
       color: "#fff"
     }
   },
   loadingWrapper: {
     textAlign: "center",
     marginTop: theme.spacing(2)
+  },
+  more: {
+    marginTop: theme.spacing(1),
+    textAlign: "center",
+    color: "#fff"
   }
-
 }));
 
 /**
- * 
+ *
  * @param {*} props data:[{label:'',value:"",id:"",icon:"",children:[{label:"",value}]}]
  */
 const StyledTreeView = props => {
   const classes = useStyles();
-  const { data, total, handleMenuClick, handleRefresh = () => { }, handleSearchVectors = () => { }, expanded, setExpanded, activeId, loading = false } = props
-  const handleClick = (nodeId, url, name) => {
+  const {
+    data,
+    total,
+    handleMenuClick,
+    handleRefresh = () => {},
+    handleSearchVectors = () => {},
+    expanded,
+    setExpanded,
+    activeId,
+    loadedAllCollection
+  } = props;
+  const handleClick = (nodeId, url, name, more) => {
     // setAcitveId(nodeId || '')
-    handleMenuClick(url, name, nodeId)
-  }
-  const generateTreeItem = (data) => {
+    handleMenuClick(url, name, nodeId, more);
+  };
+  const generateTreeItem = data => {
     return data.map(v => {
-      const comp = <StyledTreeItem
-        key={`${v.id}`}
-        nodeId={`${v.id}`}
-        labelText={v.label}
-        labelIcon={v.icon}
-        labelInfo={v.value}
-        disabled={v.disabled}
-        url={v.url}
-        searchUrl={v.searchUrl}
-        propsClick={handleClick}
-        activeId={activeId}
-        iconBtn={v.iconBtn || false}
-        needHover={v.needHover || false}
-        propsIconBtnClick={handleSearchVectors}
-      >
-        {
-          v.children && v.children.length && generateTreeItem(v.children)
-        }
-      </StyledTreeItem>
-      return comp
-    })
-  }
+      const comp = (
+        <StyledTreeItem
+          key={`${v.id}`}
+          nodeId={`${v.id}`}
+          labelText={v.label}
+          labelIcon={v.icon}
+          labelInfo={v.value}
+          disabled={v.disabled}
+          url={v.url}
+          searchUrl={v.searchUrl}
+          propsClick={handleClick}
+          activeId={activeId}
+          iconBtn={v.iconBtn || false}
+          needHover={v.needHover || false}
+          propsIconBtnClick={handleSearchVectors}
+          moreType={v.moreType}
+        >
+          {v.children && v.children.length && generateTreeItem(v.children)}
+        </StyledTreeItem>
+      );
+      return comp;
+    });
+  };
 
   const handleNodeToggle = (e, nodeIds) => {
-    console.log(nodeIds)
     const copy = nodeIds.filter((v, i) => {
       // the newer
       if (i === 0) {
-        return true
+        return true;
       }
-      // collections 
-      if (v === '1') {
-        return true
+      // collections
+      if (v === "1") {
+        return true;
       }
       // the older
-      return false
-    })
-    setExpanded(copy.length ? copy : ['1'])
-  }
+      return false;
+    });
+    setExpanded(copy.length ? copy : ["1"]);
+  };
 
   return (
     <TreeView
@@ -96,14 +117,21 @@ const StyledTreeView = props => {
         needHover={true}
         url="/data/collections"
       >
-        {
-          loading
-            ? (<div className={classes.loadingWrapper}><CircularProgress size={30}></CircularProgress></div>)
-            : generateTreeItem(data)
-        }
+        {generateTreeItem(data)}
+        {loadedAllCollection ? (
+          <div></div>
+        ) : (
+          <StyledTreeItem
+            nodeId="more collections"
+            labelText="More Collections"
+            labelIcon={FiPlus}
+            propsClick={handleClick}
+            moreType="collections"
+          ></StyledTreeItem>
+        )}
       </StyledTreeItem>
     </TreeView>
   );
-}
+};
 
-export default StyledTreeView
+export default StyledTreeView;
