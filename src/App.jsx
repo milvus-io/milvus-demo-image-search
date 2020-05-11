d0032f10359bfd10583e20ad8c90f7287e32a66f
chenzizhao <nameczz@126.com>
2020-05-11 10:21:29 +0800

    test action

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import HashRouter from './Router';
-import { HttpProvider } from './context/Http'
-import { RootProvider } from './context/Root'
+import HashRouter from "./Router";
+import { HttpProvider } from "./context/Http";
+import { RootProvider } from "./context/Root";
 import "./app.less";
-import "./assets/scss/reset.less"
+import "./assets/scss/reset.less";
 
 /**
- *  SystemProvider will always on top. The currenAddress means selected milvus ip.
- *  So the other context data must like:
- *  {
- *    '127.0.0.1:19121': {...data},
- *    '127.0.0.1:19122': {...data},
- *  }
- *  
- *  Then we can get current data by currenAddress!
- *  The currenAddress also need store in localstorage for http context to use it.
+image search
  */
 export default function App() {
   return (
