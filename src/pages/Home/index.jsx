55500c9cc53e52cc68c3e4b9ae88d79081505239
chenzizhao <nameczz@126.com>
2020-05-25 14:09:06 +0800

    test branch action

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,61 +1,61 @@
-import React, { useEffect, useState, useRef, useContext } from 'react'
-import FileDrop from 'react-file-drop'
-import Gallery from '../../components/Gallery'
-import Cropper from '../../components/Cropper'
-import { getImgUrl } from '../../utils/helpers'
-import { CircularProgress, useMediaQuery, IconButton } from '@material-ui/core'
-import { VerticalAlignTop } from '@material-ui/icons'
-import { Search } from '@material-ui/icons'
-import { httpContext } from '../../context/Http'
-import "./index.less"
-import DemoImg from '../../assets/demo.jpg'
-import { getBase64Image, convertBase64UrlToBlob } from '../../utils/helpers'
-let timer = null
-const Home = props => {
+import React, { useEffect, useState, useRef, useContext } from "react";
+import FileDrop from "react-file-drop";
+import Gallery from "../../components/Gallery";
+import Cropper from "../../components/Cropper";
+import { getImgUrl } from "../../utils/helpers";
+import { CircularProgress, useMediaQuery, IconButton } from "@material-ui/core";
+import { VerticalAlignTop } from "@material-ui/icons";
+import { Search } from "@material-ui/icons";
+import { httpContext } from "../../context/Http";
+import "./index.less";
+import DemoImg from "../../assets/demo.jpg";
+import { getBase64Image, convertBase64UrlToBlob } from "../../utils/helpers";
+let timer = null;
+const Home = (props) => {
   const isMobile = !useMediaQuery("(min-width:1000px)");
-  const [show, setShow] = useState(false)
-  const [imgs, setImgs] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [globalLoading, setGlobalLoading] = useState(false)
-  const [selectedImg, setSelectedImg] = useState("")
-  const [blob, setBlob] = useState("")
-  const [page, setPage] = useState(0)
-  const [noData, setNoData] = useState(false)
-  const dropRef = useRef(null)
-  const inputRef = useRef(null)
-  const imgsWrapperRef = useRef(null)
-  const { search } = useContext(httpContext)
+  const [show, setShow] = useState(false);
+  const [imgs, setImgs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [globalLoading, setGlobalLoading] = useState(false);
+  const [selectedImg, setSelectedImg] = useState("");
+  const [blob, setBlob] = useState("");
+  const [page, setPage] = useState(0);
+  const [noData, setNoData] = useState(false);
+  const dropRef = useRef(null);
+  const inputRef = useRef(null);
+  const imgsWrapperRef = useRef(null);
+  const { search } = useContext(httpContext);
 
   const handleDrop = (files, event) => {
     if (!files[0]) {
-      return
+      return;
     }
-    const src = getImgUrl(files[0])
-    setSelectedImg(files[0] ? src : "")
-    setShow(false)
-  }
+    const src = getImgUrl(files[0]);
+    setSelectedImg(files[0] ? src : "");
+    setShow(false);
+  };
   useEffect(() => {
+    console.log("test action");
     window.addEventListener("dragover", (e) => {
       // This prevents the browser from trying to load whatever file the user dropped on the window
       e.preventDefault();
-      setShow(true)
-    })
+      setShow(true);
+    });
     window.addEventListener("dragleave", (e) => {
       e.preventDefault();
-      setShow(false)
-    })
+      setShow(false);
+    });
     window.addEventListener("drop", (e) => {
       e.preventDefault();
-      const inArea = e.path.includes(dropRef.current)
+      const inArea = e.path.includes(dropRef.current);
       if (!inArea) {
-        setShow(false)
+        setShow(false);
       }
-    })
-
-  }, [])
+    });
+  }, []);
   useEffect(() => {
     const image = new Image();
-    image.crossOrigin = '';
+    image.crossOrigin = "";
     image.src = DemoImg;
     image.onload = function () {
       const base64 = getBase64Image(image);
@@ -67,125 +67,149 @@ const Home = props => {
        }
        */
       const imgBlob = convertBase64UrlToBlob(base64);
-      handleImgSearch(imgBlob)
+      handleImgSearch(imgBlob);
       /*
        打印信息如下：
        Blob {size: 9585, type: "image/jpg"}
        */
-    }
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
-  const handleScroll = async e => {
-    const { scrollTop, offsetHeight, scrollHeight } = e.currentTarget
-    if (scrollHeight - scrollTop - offsetHeight < 30 && !loading && !timer && imgs.length) {
+  const handleScroll = async (e) => {
+    const { scrollTop, offsetHeight, scrollHeight } = e.currentTarget;
+    if (
+      scrollHeight - scrollTop - offsetHeight < 30 &&
+      !loading &&
+      !timer &&
+      imgs.length
+    ) {
       timer = setTimeout(async () => {
-        setLoading(true)
-        setPage(v => v + 1)
-        await handleImgSearch(blob, false)
-        timer = null
-      }, 100)
-
+        setLoading(true);
+        setPage((v) => v + 1);
+        await handleImgSearch(blob, false);
+        timer = null;
+      }, 100);
     }
-  }
+  };
 
   const handleInput = () => {
-    inputRef.current.click()
-  }
+    inputRef.current.click();
+  };
 
   const handleInputChange = async (e) => {
-    const file = inputRef.current.files[0] || ""
+    const file = inputRef.current.files[0] || "";
     if (!file) {
-      return
+      return;
     }
-    const src = getImgUrl(file)
-    setSelectedImg(file ? src : "")
-  }
-
+    const src = getImgUrl(file);
+    setSelectedImg(file ? src : "");
+  };
 
   const handleImgSearch = async (file, reset = true) => {
     if (reset) {
-      setImgs([])
-      setPage(0)
-      setGlobalLoading(true)
-      setNoData(false)
-
+      setImgs([]);
+      setPage(0);
+      setGlobalLoading(true);
+      setNoData(false);
     }
-    const fd = new FormData()
-    fd.append("file", file)
-    fd.append("Num", 50)
-    fd.append("Page", page)
+    const fd = new FormData();
+    fd.append("file", file);
+    fd.append("Num", 50);
+    fd.append("Page", page);
 
-    setBlob(file)
-    const res = await search(fd)
+    setBlob(file);
+    const res = await search(fd);
     if (!res.length) {
-      setNoData(true)
-      return
+      setNoData(true);
+      return;
     }
-    setImgs(v => ([
+    setImgs((v) => [
       ...v,
-      ...res.map(v => ({
+      ...res.map((v) => ({
         src: v[0],
-        distance: v[1]
-      }))
-    ]))
-  }
-  const handleSearch = src => {
-    setSelectedImg(src)
-  }
+        distance: v[1],
+      })),
+    ]);
+  };
+  const handleSearch = (src) => {
+    setSelectedImg(src);
+  };
 
   const handleBackToTop = () => {
-    document.querySelector(".route-section").scrollTop = 0
-    imgsWrapperRef.current.scrollTop = 0
-  }
+    document.querySelector(".route-section").scrollTop = 0;
+    imgsWrapperRef.current.scrollTop = 0;
+  };
 
-  return <div className="home-wrapper" >
-    <div className={`file-drop ${show && 'open'}`} ref={dropRef}>
-      <FileDrop onDrop={handleDrop} className="target" >
-        <div className="tip" >Drop Image here!</div>
-      </FileDrop>
-    </div>
-    <div className={`cropper-wrapper ${selectedImg && 'show'}`}>
-      <Cropper src={selectedImg} propSend={handleImgSearch} className="crop-img-wrapper" imgClassName="crop-img"></Cropper>
-    </div>
-    <div className="imgs-wrapper" ref={imgsWrapperRef} onScroll={handleScroll}>
-      {
-        imgs.length ? <Gallery
-          handleSearch={handleSearch}
-          setLoading={setLoading}
-          setGlobalLoading={setGlobalLoading}
-          imgs={imgs}
-        ></Gallery> : ""
-      }
-      {
-        globalLoading && (<div className="global-loading-wrapper">
-          <CircularProgress></CircularProgress>
-        </div>)
-      }
-      {
-        loading && !noData && (<div className="loading-wrapper">
-          <CircularProgress></CircularProgress>
-        </div>)
-      }
-      {
-        noData && (<p style={{ textAlign: "center" }}>No More Data.</p>)
-      }
-    </div>
-    <span className="upload" onClick={handleInput}>
-      <Search></Search>
-    </span>
-    <input type="file" ref={inputRef} className="input" onChange={handleInputChange}></input>
-    {
-      isMobile && (
+  return (
+    <div className="home-wrapper">
+      <div className={`file-drop ${show && "open"}`} ref={dropRef}>
+        <FileDrop onDrop={handleDrop} className="target">
+          <div className="tip">Drop Image here!</div>
+        </FileDrop>
+      </div>
+      <div className={`cropper-wrapper ${selectedImg && "show"}`}>
+        <Cropper
+          src={selectedImg}
+          propSend={handleImgSearch}
+          className="crop-img-wrapper"
+          imgClassName="crop-img"
+        ></Cropper>
+      </div>
+      <div
+        className="imgs-wrapper"
+        ref={imgsWrapperRef}
+        onScroll={handleScroll}
+      >
+        {imgs.length ? (
+          <Gallery
+            handleSearch={handleSearch}
+            setLoading={setLoading}
+            setGlobalLoading={setGlobalLoading}
+            imgs={imgs}
+          ></Gallery>
+        ) : (
+          ""
+        )}
+        {globalLoading && (
+          <div className="global-loading-wrapper">
+            <CircularProgress></CircularProgress>
+          </div>
+        )}
+        {loading && !noData && (
+          <div className="loading-wrapper">
+            <CircularProgress></CircularProgress>
+          </div>
+        )}
+        {noData && <p style={{ textAlign: "center" }}>No More Data.</p>}
+      </div>
+      <span className="upload" onClick={handleInput}>
+        <Search></Search>
+      </span>
+      <input
+        type="file"
+        ref={inputRef}
+        className="input"
+        onChange={handleInputChange}
+      ></input>
+      {isMobile && (
         <IconButton
           onClick={handleBackToTop}
           color="primary"
-          style={{ position: "fixed", bottom: "20px", right: "20px", borderRadius: "50%", background: "#2196f3", color: "#fff" }}>
+          style={{
+            position: "fixed",
+            bottom: "20px",
+            right: "20px",
+            borderRadius: "50%",
+            background: "#2196f3",
+            color: "#fff",
+          }}
+        >
           <VerticalAlignTop></VerticalAlignTop>
         </IconButton>
-      )
-    }
-  </div>
-}
+      )}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
