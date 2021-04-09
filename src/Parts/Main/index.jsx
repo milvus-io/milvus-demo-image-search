d2647eda8cafd223c25ba3b51766234fdf2e6701
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2021-04-09 18:39:38 +0800

    update images

--- src/Parts/Main/index.jsx
+++ src/Parts/Main/index.jsx
@@ -2,49 +2,34 @@ import React from "react";
 import { Masonry } from "gestalt";
 import Item from "../Item";
 
-const pins = [
-  { id: 1, name: "text" },
-  { id: 2, name: "te2" },
-  { id: 3, name: "te3" },
-  { id: 4, name: "te4" },
-  { id: 5, name: "te5" },
-  { id: 1, name: "text" },
-  { id: 2, name: "te2" },
-  { id: 3, name: "te3" },
-  { id: 4, name: "te4" },
-  { id: 5, name: "te5" },
-  { id: 1, name: "text" },
-  { id: 2, name: "te2" },
-  { id: 3, name: "te3" },
-  { id: 4, name: "te4" },
-  { id: 5, name: "te5" },
-  { id: 1, name: "text" },
-  { id: 2, name: "te2" },
-  { id: 3, name: "te3" },
-  { id: 4, name: "te4" },
-  { id: 5, name: "te5" },
-  { id: 1, name: "text" },
-  { id: 2, name: "te2" },
-  { id: 3, name: "te3" },
-  { id: 4, name: "te4" },
-  { id: 5, name: "te5" },
-];
 
+let offset = 0;
 function loadItems() {
-  console.log('load')
-  return [];
+  console.log("load");
+  let res =  new Array(20).fill(true).map((d, index) => {
+    return { id: offset + index, name: Math.random() * 10000 };
+  });
+  offset += 20;
+  return res;
 }
 
+let pins = loadItems();
+
 function Main() {
+  const scrollContainer = React.useRef();
   return (
-    <Masonry
-      flexible
-      virtualize
-      comp={Item}
-      items={pins}
-      loadItems={loadItems}
-      minCols={1}
-    ></Masonry>
+    <div ref={scrollContainer}>
+      <Masonry
+        flexible={true}
+        virtualize={true}
+        comp={Item}
+        items={pins}
+        gutterWidth={0}
+        loadItems={loadItems}
+        scrollContainer={() => scrollContainer.current}
+        minCols={1}
+      ></Masonry>
+    </div>
   );
 }
 
